fbbfb5949d593d0304c9da07b1594c7e5601c036
Eliseu Medeiros <eliseujayro@gmail.com>
2023-08-20 23:44:00 -0300

    Simple create form

--- SalesWebMvcApp/Controllers/SellersController.cs
+++ SalesWebMvcApp/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvcApp.Models;
 using SalesWebMvcApp.Services;
 
 
@@ -17,6 +18,19 @@ namespace SalesWebMvcApp.Controllers
         {
             var list = _sellerService.findAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
