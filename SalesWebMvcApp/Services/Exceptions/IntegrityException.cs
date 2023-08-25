d3259a046d7be6a99f81c92591ca27f828fee1c2
Eliseu Medeiros <eliseujayro@gmail.com>
2023-08-24 22:07:04 -0300

    Exception handling for delete (referential integrity)

---
+++ SalesWebMvcApp/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMvcApp.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+    }
+}
